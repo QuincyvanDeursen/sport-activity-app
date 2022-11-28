a1b4cba0eb6095e3c2811c4257c79036940a1dae
Quincy van Deursen <q.vandeursen@student.avans.nl>
2022-11-28 21:40:20 +0100

    NestJs init

---
+++ apps/sport-activity-api/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'sport-activity-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/sport-activity-api',
+};
---
+++ libs/domain/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/domain.module';
+export * from './lib/Role';
+export * from './lib/User';
